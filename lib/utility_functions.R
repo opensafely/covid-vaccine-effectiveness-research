03a1988b95a900158bf844d282cb9822a7696f7c
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-02-15 10:43:28 +0000

    add round and cut functions

--- lib/utility_functions.R
+++ lib/utility_functions.R
@@ -10,6 +10,8 @@ fct_case_when <- function(...) {
   factor(dplyr::case_when(...), levels=levels)
 }
 
+
+
 censor <- function(event_date, censor_date, na.censor=TRUE){
   # censors event_date to on or before censor_date
   # if na.censor = TRUE then returns NA if event_date>censor_date, otherwise returns min(event_date, censor_date)
@@ -32,4 +34,37 @@ tte <- function(origin_date, event_date, censor_date, na.censor=FALSE){
   else
     time <- pmin(event_date-origin_date, censor_date-origin_date, na.rm=TRUE)
     as.numeric(time)
+}
+
+
+
+round_tte <- function(x, width=7){
+  # group follow-up time to be in periods of size `width`
+  # eg, convert to weekly instead of dail with width=7
+  # follow-up time of zero is always mapped to zero
+  # then first period is mapped to `1`, second period is mapped to `2`, etc
+  as.numeric(ggplot2::cut_width(x, width=width, labels=FALSE, boundary=width+1, closed="left")-1L)
+}
+
+
+postvax_cut <- function(event_time, time, breaks, prelabel="pre", prefix=""){
+
+  # this function defines post-vaccination time-periods at `time`,
+  # for a vaccination occurring at time `event_time`
+  # delimited by `breaks`
+
+  # note, intervals are open on the left and closed on the right
+  # so at the exact time point the vaccination occurred, it will be classed as "pre-dose".
+
+  event_time <- as.numeric(event_time)
+  event_time <- if_else(!is.na(event_time), event_time, Inf)
+
+  diff <- time - event_time
+  breaks_aug <- unique(c(-Inf, breaks, Inf))
+  labels0 <- cut(c(breaks, Inf), breaks_aug)
+  labels <- paste0(prefix, c(prelabel, as.character(labels0[-1])))
+  period <- cut(diff, breaks=breaks_aug, labels=labels, include.lowest=TRUE)
+
+
+  period
 }
