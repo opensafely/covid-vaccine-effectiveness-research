30a2f1b75c1712fcefad2a103c65e387f2a7e99d
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-02-18 20:09:14 +0000

    improve scale, add rates

--- analysis/R/plot_tests_per_day.R
+++ analysis/R/plot_tests_per_day.R
@@ -72,21 +72,86 @@ data_pt <- read_rds(here::here("output", "modeldata", glue::glue("data_pt_{cohor
 
 
 postestsperday <- data_pt %>%
-  group_by(tstop, vax_status) %>%
-  summarise(
+  mutate(
     date=as.Date(gbl_vars$start_date) + tstop,
+  ) %>%
+  group_by(date, vax_status) %>%
+  summarise(
     n=n(),
-    positive_tests = sum(outcome)
+    n_positive_tests = sum(outcome),
+    rate_positive_tests = mean(outcome)
   )
 
 
-plotpostestsperday <- ggplot(postestsperday) +
-  geom_bar(aes(x=date, y=positive_tests, fill=vax_status==1), stat="identity", colour="transparent")+
+plotpostestsperday_n <- ggplot(postestsperday) +
+  geom_bar(aes(x=date, y=n_positive_tests, fill=vax_status==1), stat="identity", colour="transparent")+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_fill_brewer(type="qual", palette="Set1", na.value="grey")+
   labs(
     x=NULL,
-    y=NULL,
+    y="Positive tests per day",
+    fill="Vaccinated",
+    title="Daily positive tests",
+    subtitle=cohort_descr
+  )+
+  theme_minimal()+
+  theme(
+    legend.position = "right",
+    strip.text.y.right = element_text(angle = 0),
+    axis.line.x = element_line(colour = "black"),
+    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday_n.svg"), plotpostestsperday_n, width=30, height=20, units="cm", scale=0.8)
+
+
+plotpostestsperday_rate <- ggplot(postestsperday) +
+  geom_line(aes(x=date, y=rate_positive_tests, colour=vax_status==1), stat="identity")+
+  scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
+  scale_colour_brewer(type="qual", palette="Set1", na.value="grey")+
+  labs(
+    x=NULL,
+    y="Positive test rate",
+    fill="Vaccinated",
+    title="Daily positive test rate",
+    subtitle=cohort_descr
+  )+
+  theme_minimal()+
+  theme(
+    legend.position = "right",
+    strip.text.y.right = element_text(angle = 0),
+    axis.line.x = element_line(colour = "black"),
+    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday_rate.svg"), plotpostestsperday_rate, width=30, height=20, units="cm", scale=0.8)
+
+
+
+
+postestsperday_sex_imd <- data_pt %>%
+  mutate(
+    date=as.Date(gbl_vars$start_date) + tstop,
+  ) %>%
+  group_by(date, vax_status, sex, imd) %>%
+  summarise(
+    n=n(),
+    n_positive_tests = sum(outcome),
+    rate_positive_tests = mean(outcome)
+  )
+
+
+plotpostestsperday_n_sex_ind <- ggplot(postestsperday_sex_imd) +
+  geom_line(aes(x=date, y=n_positive_tests, colour=vax_status==1), stat="identity")+
+  facet_grid(cols=vars(sex), rows=vars(imd), margins=TRUE)+
+  scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
+  scale_colour_brewer(type="qual", palette="Set1", na.value="grey")+
+  labs(
+    x=NULL,
+    y="Positive tests per day",
     fill="Vaccinated",
     title="Daily positive tests",
     subtitle=cohort_descr
@@ -102,28 +167,20 @@ plotpostestsperday <- ggplot(postestsperday) +
   )
 
 
-ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday.svg"), plotpostestsperday, width=20, height=30, units="cm")
-
-postestsperday_sex_imd <- data_pt %>%
-  group_by(tstop, vax_status, sex, imd) %>%
-  summarise(
-    date=as.Date(gbl_vars$start_date) + tstop,
-    n=n(),
-    positive_tests = sum(outcome)
-  )
+ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday_n_sex_imd.svg"), plotpostestsperday_n_sex_ind, width=30, height=40, units="cm", scale=0.6)
 
 
 
-plotpostestsperday_age_ind <- ggplot(postestsperday_sex_imd) +
-  geom_bar(aes(x=date, y=positive_tests, fill=vax_status==1), stat="identity", colour="transparent")+
+plotpostestsperday_rate_sex_ind <- ggplot(postestsperday_sex_imd) +
+  geom_bar(aes(x=date, y=rate_positive_tests, fill=vax_status==1), stat="identity", colour="transparent")+
   facet_grid(cols=vars(sex), rows=vars(imd), margins=TRUE)+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_fill_brewer(type="qual", palette="Set1", na.value="grey")+
   labs(
     x=NULL,
-    y=NULL,
+    y="Positive test rate",
     fill="Vaccinated",
-    title="Daily positive tests",
+    title="Daily positive test rate",
     subtitle=cohort_descr
   )+
   theme_minimal()+
@@ -137,4 +194,5 @@ plotpostestsperday_age_ind <- ggplot(postestsperday_sex_imd) +
   )
 
 
-ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday_sex_imd.svg"), plotpostestsperday_age_ind, width=20, height=30, units="cm")
+ggsave(filename=here::here("output", "models", "msm", cohort, "postestsperday_rate_sex_imd.svg"), plotpostestsperday_rate_sex_ind, width=30, height=40, units="cm", scale=0.8)
+
