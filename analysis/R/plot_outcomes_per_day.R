9dbb7197b55c7cb9aafdc6068a6a044529685f14
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-03-01 21:54:23 +0000

    automate events per day for outcomes too

--- analysis/R/plot_outcomes_per_day.R
+++ analysis/R/plot_outcomes_per_day.R
@@ -2,14 +2,12 @@
 # # # # # # # # # # # # # # # # # # # # #
 # This script:
 # takes a cohort name as defined in data_define_cohorts.R, and imported as an Arg
-# creates 3 datasets for that cohort:
-# 1 is one row per patient (wide format)
-# 2 is one row per patient per event (eg `stset` format, where a new row is created everytime an event occurs or a covariate changes)
-# 3 is one row per patient per day
-# creates additional survival variables for use in models (eg time to event from study start date)
+# creates descriptive outputs on the occurrence of outcome events over time
 #
-# The script should only be run via an action in the project.yaml only
-# The script must be accompanied by one argument, the name of the cohort defined in data_define_cohorts.R
+# The script should be run via an action in the project.yaml
+# The script must be accompanied by two arguments,
+# 1. the name of the cohort defined in data_define_cohorts.R
+# 2. the name of the outcome defined in data_define_cohorts.R
 # # # # # # # # # # # # # # # # # # # # #
 
 # Preliminaries ----
@@ -19,73 +17,97 @@ library('tidyverse')
 library('lubridate')
 library('survival')
 library('splines')
-library('parglm')
 
 ## Import custom user functions from lib
 source(here::here("lib", "utility_functions.R"))
 source(here::here("lib", "redaction_functions.R"))
 source(here::here("lib", "survival_functions.R"))
 
-# import command-line arguments ----
+## import command-line arguments ----
 
 args <- commandArgs(trailingOnly=TRUE)
 
-cohort <- args[[1]]
 
 if(length(args)==0){
   # use for interactive testing
   cohort <- "over80s"
+  outcome <- "postest"
+} else {
+  # use for actions
+  cohort <- args[[1]]
+  outcome <- args[[2]]
 }
 
-# import global vars ----
+## import global vars ----
 gbl_vars <- jsonlite::fromJSON(
-  txt="./lib/global-variables.json"
+  txt="./analysis/global-variables.json"
 )
 #list2env(gbl_vars, globalenv())
 
 
-# Import metadata for cohort ----
+## Import metadata for cohort ----
 
-metadata_cohorts <- read_rds(here::here("output", "modeldata", "metadata_cohorts.rds"))
-metadata_cohorts <- metadata_cohorts[metadata_cohorts[["cohort"]]==cohort,]
+metadata_cohorts <- read_rds(here::here("output", "data", "metadata_cohorts.rds"))
+metadata <- metadata_cohorts[metadata_cohorts[["cohort"]]==cohort & metadata_cohorts[["outcome"]]==outcome, ]
+
 
 stopifnot("cohort does not exist" = (cohort %in% metadata_cohorts[["cohort"]]))
 
-## define model hyper-parameters and characteristics ----
 
-### model names ----
-
-list2env(metadata_cohorts, globalenv())
-
-## or equivalently:
+## put metadata into global environment
+list2env(metadata, globalenv())
+### or equivalently:
 # cohort <- metadata_cohorts$cohort
 # cohort_descr <- metadata_cohorts$cohort_descr
 # outcome <- metadata_cohorts$outcome
 # outcome_descr <- metadata_cohorts$outcome_descr
 
-# create output directories ----
-dir.create(here::here("output", "models", "msm", cohort), showWarnings = FALSE, recursive=TRUE)
-
-# Import processed data ----
-
-data_pt <- read_rds(here::here("output", "modeldata", glue::glue("data_pt_{cohort}.rds")))
 
 
-postestsperday <- data_pt %>%
+
+## create output directories ----
+dir.create(here::here("output", cohort, outcome, "descr"), showWarnings = FALSE, recursive=TRUE)
+
+
+## Import processed data ----
+
+data_pt <- read_rds(here::here("output", cohort, outcome, glue::glue("data_pt.rds")))
+
+
+## define theme ----
+
+plot_theme <-
+theme_minimal()+
+  theme(
+    legend.position = "left",
+    strip.text.y.right = element_text(angle = 0),
+    axis.line.x = element_line(colour = "black"),
+    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank()
+  )
+
+
+# create plots ----
+
+## overall ----
+
+eventsperday <- data_pt %>%
   mutate(
-    date=as.Date(gbl_vars$start_date) + tstop,
+    outcome = .[[outcome]],
+    date = as.Date(gbl_vars$start_date) + tstop,
     anyvax = vax_status>0
   ) %>%
   group_by(date, anyvax) %>%
   summarise(
     n=n(),
-    n_positive_tests = sum(outcome),
-    rate_positive_tests = mean(outcome)
+    n_outcome = sum(outcome),
+    rate_outcome = mean(outcome)
   )
 
 
-plotpostestsperday_n <- ggplot(postestsperday) +
-  geom_bar(aes(x=date, y=n_positive_tests, fill=anyvax), stat="identity", colour="transparent")+
+plotoutcome_n <- ggplot(eventsperday) +
+  geom_bar(aes(x=date, y=n_outcome, fill=anyvax), stat="identity", colour="transparent")+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_fill_brewer(type="qual", palette="Set1", na.value="grey")+
   labs(
@@ -95,20 +117,13 @@ plotpostestsperday_n <- ggplot(postestsperday) +
     title=glue::glue("{outcome_descr} frequency"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "right",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_n.svg")), plotpostestsperday_n, width=30, height=20, units="cm", scale=0.8)
+  plot_theme
+
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_n.svg")), plotoutcome_n, width=30, height=20, units="cm", scale=0.8)
 
 
-plotpostestsperday_rate <- ggplot(postestsperday) +
-  geom_line(aes(x=date, y=rate_positive_tests, colour=anyvax), stat="identity")+
+plotoutcome_rate <- ggplot(eventsperday) +
+  geom_line(aes(x=date, y=rate_outcome, colour=anyvax), stat="identity")+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_colour_brewer(type="qual", palette="Set1", na.value="grey")+
   labs(
@@ -118,22 +133,14 @@ plotpostestsperday_rate <- ggplot(postestsperday) +
     title=glue::glue("{outcome_descr} rate"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "right",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+  plot_theme
 
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_rate.svg")), plotpostestsperday_rate, width=30, height=20, units="cm", scale=0.8)
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_rate.svg")), plotoutcome_rate, width=30, height=20, units="cm", scale=0.8)
+
+## by sex, imd ----
 
 
-
-
-postestsperday_sex_imd <- data_pt %>%
+outcomeperday_sex_imd <- data_pt %>%
   mutate(
     date=as.Date(gbl_vars$start_date) + tstop,
     anyvax = vax_status>0
@@ -141,13 +148,13 @@ postestsperday_sex_imd <- data_pt %>%
   group_by(date, anyvax, sex, imd) %>%
   summarise(
     n=n(),
-    n_positive_tests = sum(outcome),
-    rate_positive_tests = mean(outcome)
+    n_outcome = sum(outcome),
+    rate_outcome = mean(outcome)
   )
 
 
-plotpostestsperday_n_sex_ind <- ggplot(postestsperday_sex_imd) +
-  geom_line(aes(x=date, y=n_positive_tests, fill=anyvax), stat="identity", colour="transparent")+
+plotoutcome_n_sex_ind <- ggplot(outcomeperday_sex_imd) +
+  geom_line(aes(x=date, y=n_outcome, fill=anyvax), stat="identity", colour="transparent")+
   facet_grid(cols=vars(sex), rows=vars(imd), margins=TRUE)+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_colour_brewer(type="qual", palette="Set1", na.value="grey")+
@@ -158,23 +165,15 @@ plotpostestsperday_n_sex_ind <- ggplot(postestsperday_sex_imd) +
     title=glue::glue("{outcome_descr} frequency"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "left",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+  plot_theme
 
 
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_n_sex_imd.svg")), plotpostestsperday_n_sex_ind, width=30, height=40, units="cm", scale=0.6)
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_n_sex_imd.svg")), plotoutcome_n_sex_ind, width=30, height=40, units="cm", scale=0.6)
 
 
 
-plotpostestsperday_rate_sex_ind <- ggplot(postestsperday_sex_imd) +
-  geom_line(aes(x=date, y=rate_positive_tests, fill=anyvax), stat="identity")+
+plotoutcome_rate_sex_ind <- ggplot(outcomeperday_sex_imd) +
+  geom_line(aes(x=date, y=rate_outcome, fill=anyvax), stat="identity")+
   facet_grid(cols=vars(sex), rows=vars(imd), margins=TRUE)+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_fill_brewer(type="qual", palette="Set1", na.value="grey")+
@@ -185,26 +184,18 @@ plotpostestsperday_rate_sex_ind <- ggplot(postestsperday_sex_imd) +
     title=glue::glue("{outcome_descr} rate"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "left",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+  plot_theme
 
 
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_rate_sex_imd.svg")), plotpostestsperday_rate_sex_ind, width=30, height=40, units="cm", scale=0.8)
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_rate_sex_imd.svg")), plotoutcome_rate_sex_ind, width=30, height=40, units="cm", scale=0.8)
 
 
 
 
+## by region ----
 
 
-
-postestsperday_region <- data_pt %>%
+outcomeperday_region <- data_pt %>%
   mutate(
     date=as.Date(gbl_vars$start_date) + tstop,
     anyvax = vax_status>0
@@ -212,13 +203,13 @@ postestsperday_region <- data_pt %>%
   group_by(date, anyvax, region) %>%
   summarise(
     n=n(),
-    n_positive_tests = sum(outcome),
-    rate_positive_tests = mean(outcome)
+    n_outcome = sum(outcome),
+    rate_outcome = mean(outcome)
   )
 
 
-plotpostestsperday_n_region <- ggplot(postestsperday_region) +
-  geom_bar(aes(x=date, y=n_positive_tests, fill=anyvax), stat="identity", colour="transparent")+
+plotoutome_n_region <- ggplot(outcomeperday_region) +
+  geom_bar(aes(x=date, y=n_outcome, fill=anyvax), stat="identity", colour="transparent")+
   facet_wrap(facets=vars(region))+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_colour_brewer(type="qual", palette="Set1", na.value="grey")+
@@ -229,23 +220,16 @@ plotpostestsperday_n_region <- ggplot(postestsperday_region) +
     title=glue::glue("{outcome_descr} frequency"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "left",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
-
-
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_n_region.svg")), plotpostestsperday_n_region, width=30, height=40, units="cm", scale=0.6)
+  plot_theme
 
 
 
-plotpostestsperday_rate_region <- ggplot(postestsperday_region) +
-  geom_line(aes(x=date, y=rate_positive_tests, colour=anyvax), stat="identity")+
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_n_region.svg")), plotoutome_n_region, width=30, height=40, units="cm", scale=0.6)
+
+
+
+plotoutcome_rate_region <- ggplot(outcomeperday_region) +
+  geom_line(aes(x=date, y=rate_outcome, colour=anyvax), stat="identity")+
   facet_wrap(facets=vars(region))+
   scale_x_date(date_breaks = "1 month", labels = scales::date_format("%Y-%m"))+
   scale_fill_brewer(type="qual", palette="Set1", na.value="grey")+
@@ -256,16 +240,7 @@ plotpostestsperday_rate_region <- ggplot(postestsperday_region) +
     title=glue::glue("{outcome_descr} rate"),
     subtitle=cohort_descr
   )+
-  theme_minimal()+
-  theme(
-    legend.position = "left",
-    strip.text.y.right = element_text(angle = 0),
-    axis.line.x = element_line(colour = "black"),
-    axis.text.x = element_text(angle = 70, vjust = 1, hjust=1),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank()
-  )
+  plot_theme
 
-
-ggsave(filename=here::here("output", "models", "msm", cohort, glue::glue("{outcome}_perday_rate_region.svg")), plotpostestsperday_rate_region, width=30, height=40, units="cm", scale=0.8)
+ggsave(filename=here::here("output", "models", "msm", cohort, outcome, "descr", glue::glue("events_perday_rate_region.svg")), plotoutcome_rate_region, width=30, height=40, units="cm", scale=0.8)
 
