0a8282a4c707480a96bf74c9bc9b0c173504b7ce
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-03-04 19:41:13 +0000

    add covid primary care dates to input.csv tidier

--- analysis/R/data_process_all.R
+++ analysis/R/data_process_all.R
@@ -82,6 +82,18 @@ data_extract0 <- read_csv(
     discharged_4_date = col_date(format="%Y-%m-%d"),
     discharged_5_date = col_date(format="%Y-%m-%d"),
 
+    primary_care_probable_covid_1_date = col_date(format="%Y-%m-%d"),
+    primary_care_probable_covid_2_date = col_date(format="%Y-%m-%d"),
+    primary_care_probable_covid_3_date = col_date(format="%Y-%m-%d"),
+    primary_care_probable_covid_4_date = col_date(format="%Y-%m-%d"),
+    primary_care_probable_covid_5_date = col_date(format="%Y-%m-%d"),
+
+    primary_care_suspected_covid_1_date = col_date(format="%Y-%m-%d"),
+    primary_care_suspected_covid_2_date = col_date(format="%Y-%m-%d"),
+    primary_care_suspected_covid_3_date = col_date(format="%Y-%m-%d"),
+    primary_care_suspected_covid_4_date = col_date(format="%Y-%m-%d"),
+    primary_care_suspected_covid_5_date = col_date(format="%Y-%m-%d"),
+
     covid_vax_1_date = col_date(format="%Y-%m-%d"),
     covid_vax_2_date = col_date(format="%Y-%m-%d"),
     covid_vax_3_date = col_date(format="%Y-%m-%d"),
@@ -254,7 +266,7 @@ write_rds(data_processed, here::here("output", "data", "data_all.rds"), compress
 
 
 ## create one-row-per-event datasets ----
-# for vaccination, positive test, hospitalisation/discharge, death
+# for vaccination, positive test, hospitalisation/discharge, covid in primary care, death
 
 
 data_admissions <- data_processed %>%
@@ -346,3 +358,5 @@ data_vax <- local({
 
 write_rds(data_vax, here::here("output", "data", "data_long_vax_dates.rds"), compress="gz")
 write_rds(data_admissions, here::here("output", "data", "data_long_admission_dates.rds"), compress="gz")
+write_rds(data_pr_probable_covid, here::here("output", "data", "data_long_pr_probable_covid_dates.rds"), compress="gz")
+write_rds(data_pr_suspected_covid, here::here("output", "data", "data_long_pr_suspected_covid_dates.rds"), compress="gz")
