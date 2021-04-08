2da53a8a1ac018f359b85d2e99e78c9bd8e79752
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-04-08 23:06:26 +0100

    add positive test as time-varying covariate

--- analysis/R/data_process_all.R
+++ analysis/R/data_process_all.R
@@ -351,6 +351,16 @@ data_pr_probable_covid <- data_processed %>%
   ) %>%
   arrange(patient_id, date)
 
+data_postest <- data_processed %>%
+  select(patient_id, matches("^positive\\_test\\_\\d+\\_date")) %>%
+  pivot_longer(
+    cols = -patient_id,
+    names_to = c(NA, "postest_index"),
+    names_pattern = "^(.*)_(\\d+)_date",
+    values_to = "date",
+    values_drop_na = TRUE
+  ) %>%
+  arrange(patient_id, date)
 
 data_vax <- local({
 
@@ -410,4 +420,5 @@ write_rds(data_vax, here::here("output", "data", "data_long_vax_dates.rds"), com
 write_rds(data_admissions, here::here("output", "data", "data_long_admission_dates.rds"), compress="gz")
 write_rds(data_pr_probable_covid, here::here("output", "data", "data_long_pr_probable_covid_dates.rds"), compress="gz")
 write_rds(data_pr_suspected_covid, here::here("output", "data", "data_long_pr_suspected_covid_dates.rds"), compress="gz")
+write_rds(data_postest, here::here("output", "data", "data_long_postest_dates.rds"), compress="gz")
 
