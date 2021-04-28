7fe00ba2528592f68006acc06c84d69de4c4ad7e
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-04-28 14:47:49 +0100

    first test of CE models

--- analysis/R/ce/model_cox.R
+++ analysis/R/ce/model_cox.R
@@ -98,27 +98,28 @@ data_tte <- data_tte %>%
 
     # time to last follow up day
     vax_day = tte(start_date, covid_vax_1_date, lastfup_date),
+
     tte_lastfup = tte(covid_vax_1_date, lastfup_date, lastfup_date),
 
-    tte_covidtest =tte(covid_vax_1_date, covid_test_1_date, lastfup_date, na.censor=FALSE),
+    tte_covidtest =tte(covid_vax_1_date, covid_test_1_date, lastfup_date, na.censor=TRUE),
     ind_covidtest = censor_indicator(covid_test_1_date, lastfup_date),
 
-    tte_postest = tte(covid_vax_1_date, positive_test_1_date, lastfup_date, na.censor=FALSE),
+    tte_postest = tte(covid_vax_1_date, positive_test_1_date, lastfup_date, na.censor=TRUE),
     ind_postest = censor_indicator(positive_test_1_date, lastfup_date),
 
-    tte_emergency = tte(covid_vax_1_date, emergency_1_date, lastfup_date, na.censor=FALSE),
+    tte_emergency = tte(covid_vax_1_date, emergency_1_date, lastfup_date, na.censor=TRUE),
     ind_emergency = censor_indicator(emergency_1_date, lastfup_date),
 
-    tte_covidadmitted = tte(covid_vax_1_date, covidadmitted_1_date, lastfup_date, na.censor=FALSE),
+    tte_covidadmitted = tte(covid_vax_1_date, covidadmitted_1_date, lastfup_date, na.censor=TRUE),
     ind_covidadmitted = censor_indicator(covidadmitted_1_date, lastfup_date),
 
-    tte_coviddeath = tte(covid_vax_1_date, coviddeath_date, lastfup_date, na.censor=FALSE),
+    tte_coviddeath = tte(covid_vax_1_date, coviddeath_date, lastfup_date, na.censor=TRUE),
     ind_coviddeath = censor_indicator(coviddeath_date, lastfup_date),
 
-    tte_noncoviddeath = tte(covid_vax_1_date, noncoviddeath_date, lastfup_date, na.censor=FALSE),
+    tte_noncoviddeath = tte(covid_vax_1_date, noncoviddeath_date, lastfup_date, na.censor=TRUE),
     ind_noncoviddeath = censor_indicator(noncoviddeath_date, lastfup_date),
 
-    tte_death = tte(covid_vax_1_date, death_date, lastfup_date, na.censor=FALSE),
+    tte_death = tte(covid_vax_1_date, death_date, lastfup_date, na.censor=TRUE),
     ind_death = censor_indicator(death_date, lastfup_date),
 
     all = factor("all",levels=c("all")),
@@ -140,12 +141,14 @@ data_cox <- data_fixed %>%
     )
   ) %>%
   mutate(
-    week_region = paste0(week, "__", region)
+    week_region = paste0(week, "__", region),
+    vax_az = (vax_brand=="az")*1
   ) %>%
   filter(
     !is.na(covid_vax_1_date),
     covid_vax_1_date<=lastfup_date,
-    tte_outcome>0 # necessary for filtering out bad dummy data
+    tte_outcome>0, # necessary for filtering out bad dummy data,
+    tte_lastfup>0, # necessary for filtering out bad dummy data,
   )
 
 stopifnot("there is some unvaccinated person-time" = !any(data_cox$vax_brand=="unvaccinated"))
@@ -172,33 +175,80 @@ for(stratum in strata){
   data_cox_sub <- data_cox %>% filter(.[[strata_var]] == stratum)
 
 
-  data_cox_sub_split <- data_cox_sub %>%
-    survSplit(
-      Surv(tte_outcome, ind_outcome) ~ .,
-      data = .,
-      cut = postvaxcuts[-1],
-      episode = "tgroup",
-      start="tstart",
-      end="tstop"
+  calendar_time <- data_cox_sub %>%
+    select(patient_id, vax_day, tte_enddate) %>%
+    mutate(
+      calendar_day = map2(vax_day, tte_enddate, ~.x:.y)
+    ) %>%
+    unnest(c(calendar_day)) %>%
+    mutate(
+      calendar_week = paste0("week ", str_pad(floor(calendar_day/7), 2, pad = "0")),
+      fup_day = calendar_day - vax_day
+    )
+
+
+  calendar_week <- calendar_time %>%
+    group_by(patient_id, calendar_week) %>%
+    filter(first(calendar_day)==calendar_day) %>%
+    ungroup()
+
+  postvax_time <- data_cox_sub %>%
+    select(patient_id) %>%
+    mutate(
+      fup_day = list(postvaxcuts),
+      timesincevax = map(fup_day, ~droplevels(timesince_cut(.x+0.5, postvaxcuts, "blah")))
+    ) %>%
+    unnest(c(fup_day, timesincevax))
+
+  # create dataset that splits follow-up time by:
+  # - time since vaccination (using postvaxcuts cutoffs)
+  # - calendar week
+  data_cox_sub_split <- tmerge(
+      data1 = data_cox_sub %>% select(-starts_with("ind_"), -ends_with("_date")),
+      data2 = data_cox_sub,
+      id = patient_id,
+      tstart = 0L,
+      tstop = pmin(tte_lastfup, tte_outcome, na.rm=TRUE),
+      ind_outcome = event(tte_outcome)
+      #calendar_week = tdc(fup_day, calendar_week)
+    ) %>%
+    tmerge(
+      data1 = .,
+      data2 = calendar_week,
+      id = patient_id,
+      calendar_week = tdc(fup_day, calendar_week)
+    ) %>%
+    tmerge(
+      data1 = .,
+      data2 = postvax_time,
+      id = patient_id,
+      timesincevax = tdc(fup_day, timesincevax)
     ) %>%
     mutate(
-      timesincevax = str_pad(tstart, 3, pad="0"),
-      vax_az = (vax_brand=="az")*1
+      week_region = paste0(calendar_week, "__", region),
     )
+
   # Time-dependent Cox models ----
 
   # calculate calendar week (this is the date of vaccination (x) + follow-up time (t), then rounded)
   tt_week <- function(x, t, ...){
     day <- x + t
-    paste0("week ", str_pad(floor(day/7), 2, pad = "0"))
+
   }
-  # should think about converting this to spline
+
+  # print(table(data_cox_sub_split$timesincevax, outcome = data_cox_sub_split$ind_outcome,  az= data_cox_sub_split$vax_az))
+  # # why does this not work? using timesincevax as a timevarying coefficient. Is it just a dummy data thing?
+  # coxmod00 <- coxph(
+  #   formula = Surv(tstart, tstop, ind_outcome) ~ vax_az + timesincevax,
+  #   data = data_cox_sub_split,
+  #   id = patient_id
+  # )
 
 
-  formula_vaxonly <- Surv(tstart, tstop, ind_outcome) ~ tt(vax_day) #+ vax_az:strata(week_region)
-  #formula_vaxonly <- Surv(tstart, tstop, ind_outcome) ~ vax_az:strata(timesincevax)
+  #formula_vaxonly <- Surv(tstart, tstop, ind_outcome) ~ vax_az  + vax_az:timesincevax + timesincevax
+  formula_vaxonly <- Surv(tstart, tstop, ind_outcome) ~ vax_az:strata(timesincevax)
 
-
+  formula_spacetime <- . ~ . + calendar_week + strata(region) + calendar_week:region
 
   ### model 0 - unadjusted vaccination effect model ----
   ## no adjustment variables
@@ -208,7 +258,7 @@ for(stratum in strata){
     formula = formula_vaxonly %>% update(formula_remove_strata_var),# %>% update(formula_cox_strata_var),
     data = data_cox_sub_split,
     robust = TRUE,
-    tt = tt_week,
+    #tt = tt_week,
     id = patient_id
   )
 
@@ -217,15 +267,17 @@ for(stratum in strata){
   write_rds(coxmod0, here::here("output", cohort, outcome, brand, strata_var, stratum, cox_strata_var, "modelcox0.rds"), compress="gz")
   if(removeobs) rm(coxmod0)
 
+
+
   ### model 1 - minimally adjusted vaccination effect model, baseline demographics only ----
   cat("  \n")
   cat("coxmod1 \n")
 
   coxmod1 <- coxph(
-    formula = formula_vaxonly %>% update(formula_demog) %>% update(formula_remove_strata_var),# %>% update(formula_cox_strata_var),
+    formula = formula_vaxonly %>% update(formula_spacetime) %>% update(formula_remove_strata_var),
     data = data_cox_sub_split,
     robust = TRUE,
-    tt = tt_week,
+    #tt = tt_week,
     id = patient_id
   )
 
@@ -236,15 +288,16 @@ for(stratum in strata){
 
 
 
-  ### model 2 - baseline, demographics, comorbs adjusted vaccination effect model ----
+
+  ### model 2 - minimally adjusted vaccination effect model, baseline demographics only ----
   cat("  \n")
   cat("coxmod2 \n")
 
   coxmod2 <- coxph(
-    formula = formula_vaxonly %>% update(formula_demog) %>% update(formula_comorbs) %>% update(formula_remove_strata_var),# %>% update(formula_cox_strata_var),
+    formula = formula_vaxonly %>% update(formula_spacetime) %>% update(formula_demog) %>% update(formula_remove_strata_var),# %>% update(formula_cox_strata_var),
     data = data_cox_sub_split,
     robust = TRUE,
-    tt = tt_week,
+    #tt = tt_week,
     id = patient_id
   )
 
@@ -254,6 +307,25 @@ for(stratum in strata){
   if(removeobs) rm(coxmod2)
 
 
+
+  ### model 3 - baseline, demographics, comorbs adjusted vaccination effect model ----
+  # cat("  \n")
+  # cat("coxmod3 \n")
+  #
+  # coxmod3 <- coxph(
+  #   formula = formula_vaxonly %>% update(formula_spacetime) %>% update(formula_demog) %>% update(formula_comorbs) %>% update(formula_remove_strata_var),# %>% update(formula_cox_strata_var),
+  #   data = data_cox_sub_split,
+  #   robust = TRUE,
+  #   #tt = tt_week,
+  #   id = patient_id
+  # )
+  #
+  # cat(glue::glue("coxmod3 data size = ", coxmod3$n), "\n")
+  # cat(glue::glue("memory usage = ", format(object.size(coxmod3), units="GB", standard="SI", digits=3L)), "\n")
+  # write_rds(coxmod3, here::here("output", cohort, outcome, brand, strata_var, stratum, cox_strata_var, "modelcox3.rds"), compress="gz")
+  # if(removeobs) rm(coxmod3)
+  #
+  #
 
   ## print warnings
   print(warnings())
