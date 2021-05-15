11e13fcb6d0d393e4a67903fd534cfcab9f3c128
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-05-15 20:41:11 +0100

    add ethnicity check

---
+++ check_ethnicity.R
@@ -0,0 +1,8 @@
+
+data_all <- read_rds(here::here("output", "data", "data_all.rds"))
+
+tabout<-capture.output(table("primary care eth"=data_all$ethnicity, "sus eth"=data_all$ethnicity_6_sus, useNA='ifany'))
+cat(tabout, sep="\n", file = here::here("output","data","ethnicity_table.txt"))
+
+tabout<-capture.output(table("primary care eth"=data_all$ethnicity, "sus eth"=data_all$ethnicity_6_sus, "combined eth"= data_all$ethnicity_combined, useNA='ifany'))
+cat(tabout, sep="\n", file = here::here("output","data","ethnicity_table_combined.txt"))
