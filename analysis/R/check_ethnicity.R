eda1c1401fbe01e0ef9cc40be5d5f2378aec0820
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-06-16 09:00:25 +0100

    grab script args, fix dataset name

--- analysis/R/check_ethnicity.R
+++ analysis/R/check_ethnicity.R
@@ -1,5 +1,7 @@
 library('tidyverse')
 
+
+args <- commandArgs(trailingOnly=TRUE)
 if(length(args)==0){
   # use for interactive testing
   cohort <- "over80s"
@@ -9,10 +11,10 @@ if(length(args)==0){
 }
 
 
-data_all <- read_rds(here::here("output", cohort, "data", "data_all.rds"))
+data_processed <- read_rds(here::here("output", cohort, "data", "data_processed.rds"))
 
-tabout<-capture.output(table("primary care eth"=data_all$ethnicity, "sus eth"=data_all$ethnicity_6_sus, useNA='ifany'))
+tabout<-capture.output(table("primary care eth"=data_processed$ethnicity, "sus eth"=data_processed$ethnicity_6_sus, useNA='ifany'))
 cat(tabout, sep="\n", file = here::here("output", cohort, "data","ethnicity_table.txt"))
 
-tabout<-capture.output(table("primary care eth"=data_all$ethnicity, "sus eth"=data_all$ethnicity_6_sus, "combined eth"= data_all$ethnicity_combined, useNA='ifany'))
+tabout<-capture.output(table("primary care eth"=data_processed$ethnicity, "sus eth"=data_processed$ethnicity_6_sus, "combined eth"= data_processed$ethnicity_combined, useNA='ifany'))
 cat(tabout, sep="\n", file = here::here("output", cohort, "data","ethnicity_table_combined.txt"))
